8e2c46b8436d0dba0feb8d948afb0a5c32bc9e45
Wiem Medimagh <91318202+WiemMedimagh@users.noreply.github.com>
2022-01-19 08:43:42 +0100

    Add files via upload

---
+++ NumMaxReducer.java
@@ -0,0 +1,29 @@
+package tn.isima.exercice;
+
+import java.io.IOException;
+
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+public class NumMaxReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+int min = 0;
+Text minWord = new Text();
+public void reduce(Text key,Iterable<IntWritable> values,Context context) throws IOException, InterruptedException{
+    
+	int sum =0;
+    for(IntWritable x : values){
+    	sum+= x.get();
+        }
+    if (sum<min) {
+    	min= sum;
+    	minWord.set(key);
+    }
+    }
+@Override
+protected void cleanup(Context context) throws IOException, InterruptedException {
+	System.out.print("linfluenceur ayant le plus nombre de share est "+minWord );
+    context.write(minWord, new IntWritable(min));
+}
+}
